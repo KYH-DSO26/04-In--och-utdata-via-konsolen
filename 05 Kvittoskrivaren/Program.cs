272f90578f9241caf05bd22b8cf3a3ab8b1810c3
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 11:18:39 +0300

    Efter uppgift 5

---
+++ 05 Kvittoskrivaren/Program.cs
@@ -0,0 +1,10 @@
+﻿string vara = "Kaffe";
+decimal pris = 79.50m;
+Console.Write("Produkt: ");
+Console.WriteLine(vara);
+Console.WriteLine($"Pris: {pris:C}");
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
