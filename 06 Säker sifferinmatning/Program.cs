009709d594dcf23ced28b519ae3ce865cb0e8b0a
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 11:38:14 +0300

    Efter uppgift 6

---
+++ 06 Säker sifferinmatning/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Mata in ett heltal: ");
+string inmatning = Console.ReadLine();
+
+if (int.TryParse(inmatning, out int resultat))
+{
+    Console.WriteLine($"Snyggt: Du matade in talet: {resultat}");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{inmatning} är inget giltigt heltal");
+    Console.ResetColor();   // Återställ färger
+}
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
