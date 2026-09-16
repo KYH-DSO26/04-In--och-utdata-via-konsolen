46190d885820069d45e22d0264e0b299d61214ad
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 15:07:16 +0300

    Efter avslutad lektion, alla uppgifter lösta

---
+++ 11 Lösenordsmaskaren/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Ange ditt lösenord: ");
+string lösenord = "";
+while (true)
+{
+    var tangent = Console.ReadKey(intercept: true); // intercept: true gör att tecknet inte visas i konsolen
+    if (tangent.Key == ConsoleKey.Enter) break; // Avsluta loopen om Enter trycks
+
+    else if (tangent.Key == ConsoleKey.Backspace)
+    {
+        if (lösenord.Length > 0)
+        {
+            lösenord = lösenord.Substring(0, lösenord.Length - 1);
+            Console.Write("\b \b"); // Tar bort det sista tecknet från konsolen)
+        }
+    }
+    else
+    {
+        lösenord += tangent.KeyChar; // Lägg till tecknet i lösenordet
+        Console.Write("*"); // Visa en asterisk istället för tecknet
+    }
+}
+
+Console.WriteLine($"\nLösenordet har sparats säkert!");
+Console.WriteLine($"Lösenordet du angav:\t {lösenord}");
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
---
+++ 12 Det kraschsäkra formuläret/Program.cs
@@ -0,0 +1,24 @@
+﻿double giltigtFlyttal = 0;
+bool godkändInmatning = false;
+while (!godkändInmatning)
+{
+    Console.Write("Mata in ett flyttal (t.ex. 12,34): ");
+    string indata = Console.ReadLine();
+    if (double.TryParse(indata, out giltigtFlyttal))
+    {
+        godkändInmatning = true;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Felaktigt format! Försök igen.");
+        Console.ResetColor();
+    }
+}
+Console.WriteLine($"Tack! Talet {giltigtFlyttal} sparades utan krasch.");
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
