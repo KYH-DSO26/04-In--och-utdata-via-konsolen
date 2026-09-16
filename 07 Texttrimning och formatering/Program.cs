a2091c837bede698c439287072a0b4fc0646c4df
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 11:48:37 +0300

    Efter uppgift 7

---
+++ 07 Texttrimning och formatering/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Skriv in ditt förnamn (gärna med extra mellanslag före eller efter): ");
+string råInmatning = Console.ReadLine();
+string inmatning = råInmatning.Trim(); // Tar bort mellanslag före och efter
+
+Console.WriteLine($"Hej [{inmatning}]! Din inmatning var {råInmatning.Length} tecken, men namnet är egentligen {inmatning.Length} tecken långt.");
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
