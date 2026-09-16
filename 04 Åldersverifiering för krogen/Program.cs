12aaa3949aec77131512179e4387c740a5ce0b94
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 11:05:57 +0300

    Efter uppgift 4

---
+++ 04 Åldersverifiering för krogen/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Mata in din ålder: ");
+string inmatning = Console.ReadLine();
+//int ålder = int.Parse(inmatning);
+bool ålderOk = int.TryParse(inmatning, out int ålder);
+Console.WriteLine($"int.TryParse resultat: {ålderOk}, ålder: {ålder}");
+if (ålder >= 18)
+{
+    Console.WriteLine("Välkommen in!");
+}
+else if(ålder < 0)
+{
+    Console.WriteLine("Ogiltig ålder.");
+}
+else
+{
+    Console.WriteLine("Tyvärr, du är för ung.");
+}
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
