769a811466af205b8ec624cbd411521f55086a78
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 10:00:07 +0300

    Efter uppgift 1

--- 01 Hälsningsmaskinen/Program.cs
+++ 01 Hälsningsmaskinen/Program.cs
@@ -1,8 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hej och välkommen till hälsningsmaskinen!");
+Console.Write("Vad heter du? ");
+string namn = Console.ReadLine();
+
+Console.Write("Vilken stad bor du i? ");
+string stad = Console.ReadLine();
+
+Console.WriteLine($"Hej {namn}! Hur är vädret i {stad}?");
 
 
 
 
-
-Console.Write("Tryck på en tangent för att stänga fönstret...");
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
 Console.ReadKey();
