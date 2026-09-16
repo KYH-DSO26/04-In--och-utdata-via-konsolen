72e0aa7d3dae26ca6805d38349a4e813187c641e
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 11:53:51 +0300

    Efter uppgift 8

---
+++ 08 Dynamisk titelbar/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vad ska fönstret heta? ");
+string titel = Console.ReadLine();
+
+Console.Title = titel;
+Console.WriteLine("Titta längst upp på fönsterkanten! Titeln har ändrats");
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
