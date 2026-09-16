7904cd7ab4998fdf3e2e8dcbe631ade2c52ef15d
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 10:25:49 +0300

    Efter uppgift 2

---
+++ 02 Eko-kammaren/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Skriv in ett meddelande: ");
+string meddelande = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Eko: {meddelande}");
+
+Console.ResetColor();   // Återställ färger
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
