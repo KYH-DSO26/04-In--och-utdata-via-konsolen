67a0a45fbd75c5e10e15774d06e1e80d07890ec1
Claes Engelin <claes.engelin@zocom.se>
2026-09-16 12:12:44 +0300

    Efter uppgift 9

---
+++ 09 Bakgrundsväxlaren/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();   // Rensa konsolfönstret
+
+Console.WriteLine("Nu kör vi med blå bakgrund och vit text!");
+Console.ResetColor();   // Återställ färger
+Console.BackgroundColor = ConsoleColor.Black;
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
